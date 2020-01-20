8377c7ac419d8b5ebae847d83b53d8fb13755169
Thomas <dt.deman.thomas@gmail.com>
2020-01-20 15:38:49 +0100

    portfolio done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
 const burger = document.querySelector('.burger')
 const modal = document.querySelector('#modal')
+const linksPortfolio = document.querySelectorAll('.linksPortfolio')
+const imgsDiv = document.querySelectorAll('.imgsDiv')
+const imgAnimate = document.querySelectorAll('.imgAnimate')
+
 
 burger.addEventListener('click', () => {
     burger.classList.add('animated', 'fadeOut', 'delay-1')
@@ -11,3 +15,54 @@ modal.addEventListener('click', () => {
     burger.classList.add('animated', 'fadeIn', 'delay-1')
     modal.classList.add('hidden')
 })
+
+const remove = ((a) => {
+    imgsDiv[a].classList.remove('hidden')
+})
+
+for(let i = 0; i < linksPortfolio.length; i ++) 
+    linksPortfolio[i].addEventListener('click', () => {
+        for(let i = 0; i < linksPortfolio.length; i ++)
+            linksPortfolio[i].classList.remove('hover-link')
+
+        linksPortfolio[i].classList.add('hover-link')
+        switch(i) {
+            case 0:
+                for(let i = 0; i < imgsDiv.length; i++) {
+                    imgsDiv[i].classList.remove('hidden')
+                    imgAnimate[i].classList = 'imgAnimate'
+                }
+
+                break
+            case 1:
+                for(let i = 0; i < imgsDiv.length; i++) {
+                    imgsDiv[i].classList.add('hidden')
+                    imgAnimate[i].classList = 'imgAnimate'
+                }
+                
+                remove(1)
+                remove(2)
+                break
+            case 2:
+                for(let i = 0; i < imgsDiv.length; i++) {
+                    imgsDiv[i].classList.add('hidden')
+                    imgAnimate[i].classList = 'imgAnimate'
+                }
+
+                remove(4)
+                remove(5)
+                break
+            case 3:
+                for(let i = 0; i < imgsDiv.length; i++) {
+                    imgsDiv[i].classList.add('hidden')
+                    imgAnimate[i].classList = 'imgAnimate'
+                }
+
+                remove(1)
+                remove(4)
+                remove(5)
+                break
+            default:
+                // Do nothing
+        }
+    })
